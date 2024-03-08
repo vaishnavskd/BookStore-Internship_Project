c3997c1115c878ac320242b916ff8e6abb5ebd25
Vaishnav S <109717500+vaishnavskd@users.noreply.github.com>
2024-03-08 11:16:21 +0530

    update Main.module.css, Navbar.module.css and Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,23 +4,23 @@ import { useNavigate } from 'react-router-dom'
 import styles from '../assets/css/Navbar.module.css'
 
 const Navbar = () => {
-    const navigate=useNavigate()
+    const navigate = useNavigate()
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" className={styles.navbar}>
                 <Toolbar>
-                    <Typography variant='h4' component="div" sx={{textAlign:'start', flexGrow: 1 }} className={styles.navHead}>
+                    <Typography variant='h4' component="div" sx={{ textAlign: 'start', flexGrow: 1 }} className={styles.navHead}>
                         BookLy
                     </Typography>
-                    <Button color='inherit' onClick={()=>{
+                    <Button color='inherit' onClick={() => {
                         navigate('/')
-                    }}>Home</Button>
-                    <Button color='inherit' onClick={()=>{
+                    }} sx={{ textTransform: 'capitalize' }}>Home</Button>
+                    <Button color='inherit' onClick={() => {
                         navigate('/books')
-                    }}>Books</Button>
-                    <Button color="inherit" onClick={()=>{
+                    }} sx={{ textTransform: 'capitalize' }}>Books</Button>
+                    <Button color="inherit" onClick={() => {
                         navigate('/login')
-                    }}>Login/Signup</Button>
+                    }} sx={{ textTransform: 'capitalize' }}>Login/Signup</Button>
                 </Toolbar>
             </AppBar>
         </Box>
---
+++ frontend/src/components/QuotesHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuotesHome = () => {
+  return (
+    <div>QuotesHome</div>
+  )
+}
+
+export default QuotesHome
